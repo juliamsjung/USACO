66a8a30dd21ad74d6d6e75382145f63b7571aa7b
Julia <juliamsjung@gmail.com>
2019-12-14 12:17:48 -0800

    2nd version of mixmilk done

--- src/dec2018/mixmilk.java
+++ src/dec2018/mixmilk.java
@@ -1,82 +1,77 @@
-
-package dec2018;
+//package dec2018;
 import java.io.*;
 import java.util.*;
-
 public class mixmilk {
+	
+	
 	static Scanner in;
 	static PrintWriter out;
-	static int[] max, m;
+	static int[] cap;
+	static int[] val;
 	
-	
-	public static void main(String [] args) {
+	public static void main(String[] args) {
+		
 		try {
-			
-			//Initialize in & out
-			in = new Scanner(new File("mixmilk.in"));
+			in= new Scanner (new File("mixmilk.in"));
 			out = new PrintWriter(new File("mixmilk.out"));
 			
 			init();
+			solve();
 			
+			for(int i =0; i < 3; i++) {
+				out.println(val[i]);
+			}
 			
-			//write out file
-			out.println(solve());
-			
-			out.close();
 			in.close();
-		}catch(Exception e){
+			out.close();
+		}
+		catch(Exception e) {
 			e.printStackTrace();
-			
 		}
 		
+	}
+	
+	private static void init() {
+		cap = new int[3];
+		val = new int[3];
 		
-		
-		}private static void init() {
-			max = new int [3];
-			m = new int [3];
-			max[0] = in.nextInt();
-			m[0] = in.nextInt();
-			max[1] = in.nextInt();
-			m[1] = in.nextInt();
-			max[2] = in.nextInt();
-			m[2] = in.nextInt();
+		for(int i =0; i < 3; i++) {
+			cap[i] = in.nextInt();
+			val[i] = in.nextInt();
+		}
+	}
+	
+	private static void solve() {
+		for(int i =0; i < 100; i++) {
+			
+			pour(0,1);
+			//System.out.println(i + " " + Arrays.toString(val));
+			i++;
+			if(i>=100)
+				break;
+			
+			pour(1,2);
+			//System.out.println(i + " " + Arrays.toString(val));
+			i++;
+			if(i >=100)
+				break;
+			
+			pour(2,0);
+			//System.out.println(i + " " + Arrays.toString(val));
 			
 			
 		}
-		public static String solve() {
-			for(int i =0; i < 100; i++) {
-				minseobabo(0,1);
-				i++;
-				if(i==100)
-					break;
-				minseobabo(1,2);
-				i++;
-				if(i==100)
-					break;
-				minseobabo(2,0);
-			
-				 
-			}	
-			
-			return m[0] + "\n" + m[1] + "\n" + m[2];
-			
-
-			
+	}
+	
+	private static void pour (int a , int b) {
+		if(val[a] + val[b] <= cap[b]) {
+			val[b] += val[a];
+			val[a] = 0;
 		}
-		public static void minseobabo (int a , int b ) {
-			if(m[a] + m[b] > max[b]) {
-				m[a] -= max[b] - m[b];
-				m[b] += max[b] - m[b];
-			}
-			else {
-				m[b] += m[a];
-				m[a] = 0;
-			}
+		else {
+			val[a] = val[a] + val[b] - cap [b];
+			val[b] = cap[b];
 		}
-/*
-10 3
-11 4
-12 5
- */
+	}
 
 }
---
+++ src/dec2018/mixmilk1.java
@@ -0,0 +1,82 @@
+
+package dec2018;
+import java.io.*;
+import java.util.*;
+
+public class mixmilk1 {
+	static Scanner in;
+	static PrintWriter out;
+	static int[] max, m;
+	
+	
+	public static void main(String [] args) {
+		try {
+			
+			//Initialize in & out
+			in = new Scanner(new File("mixmilk.in"));
+			out = new PrintWriter(new File("mixmilk.out"));
+			
+			init();
+			
+			
+			//write out file
+			out.println(solve());
+			
+			out.close();
+			in.close();
+		}catch(Exception e){
+			e.printStackTrace();
+			
+		}
+		
+		
+		
+		}private static void init() {
+			max = new int [3];
+			m = new int [3];
+			max[0] = in.nextInt();
+			m[0] = in.nextInt();
+			max[1] = in.nextInt();
+			m[1] = in.nextInt();
+			max[2] = in.nextInt();
+			m[2] = in.nextInt();
+			
+			
+		}
+		public static String solve() {
+			for(int i =0; i < 100; i++) {
+				minseobabo(0,1);
+				i++;
+				if(i==100)
+					break;
+				minseobabo(1,2);
+				i++;
+				if(i==100)
+					break;
+				minseobabo(2,0);
+			
+				 
+			}	
+			
+			return m[0] + "\n" + m[1] + "\n" + m[2];
+			
+
+			
+		}
+		public static void minseobabo (int a , int b ) {
+			if(m[a] + m[b] > max[b]) {
+				m[a] -= max[b] - m[b];
+				m[b] += max[b] - m[b];
+			}
+			else {
+				m[b] += m[a];
+				m[a] = 0;
+			}
+		}
+/*
+10 3
+11 4
+12 5
+ */
+
+}

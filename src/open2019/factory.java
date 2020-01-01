c3883724f4f924ed04bdedd1e1f4ac6cc5a220da
Julia <juliamsjung@gmail.com>
2019-12-31 18:09:44 -0800

    factory usaco

---
+++ src/open2019/factory.java
@@ -0,0 +1,56 @@
+//package open2019;
+import java.io.*;
+import java.util.*;
+public class factory {
+	
+	static Scanner in;
+	static PrintWriter out;
+	static int[] enter, leave;
+	static int N;
+	
+	
+	public static void main(String args[]) {
+		try {
+			in = new Scanner (new File("factory.in"));
+			out = new PrintWriter (new File("factory.out"));
+			
+			init();
+			out.println(solve());
+			
+			in.close();
+			out.close();
+		}catch(Exception e) {
+			e.printStackTrace();
+		}
+	}
+	
+	private static void init() {
+		N = in.nextInt();
+		enter = new int[N];
+		leave = new int[N];
+		
+	}
+	
+	private static int solve() {
+		for(int i =0; i < N-1; i++) {
+			leave[in.nextInt()-1]++;
+			enter[in.nextInt()-1]++;
+		}
+		int count = 0;
+		int index =0;
+		for(int i =0; i < N; i++) {
+			if(leave[i] == 0) {
+				count++;
+				index = i;
+			}
+			if(count > 1) {
+				return -1;
+			}
+		}
+		index++;
+		return index;
+		
+		
+	}
+
+}
